a0c38fc9d79f6e3dc0b8b0bb04b83bd8ce1ca219
williamqu0108 <91743958+williamqu0108@users.noreply.github.com>
2022-03-06 22:18:58 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+// File name: midterm
+// Author's name: Haoren Qu
+// student ID: 301180297
+// Web App name: booklist
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://dbwilliam:20000108@cluster0.xgwru.mongodb.net/midterm?retryWrites=true&w=majority"
+};
